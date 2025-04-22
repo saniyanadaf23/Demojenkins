1ab7c66a2b3a0f1da6cee71f690bcca09183d3ea
qanusha436 <qanusha436@gmail.com>
2025-04-22 14:26:58 +0530

    add in main

--- Student.java
+++ Student.java
@@ -1,14 +1,14 @@
 public class Student {
-public static void main (String[] args){
-    String name="saniya";
-    int sid=117;
-    int fee=75000;
-    String college="kletech";
-
-    System.out.println(x:"----------STUDENT DETAILS----------");
-    System.out.println("Student name:"+name);
-    System.out.println("Student id:"+sid);
-    System.out.println("Student fee:"+fee);
-    System.out.println("Student college:"+college);
-}
-}
+    public static void main (String[] args){
+        String name="saniya";
+        int sid=117;
+        int fee=75000;
+        String college="kletech";
+        
+        System.out.println("Student name:"+name);
+        System.out.println("Student id:"+sid);
+        System.out.println("Student fee:"+fee);
+        System.out.println("Student college:"+college);
+    }
+    }
+    
